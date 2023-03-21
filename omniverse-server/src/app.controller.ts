e94240e4b6609a997755bcf1030f7f2df09055a5
itaym <itaymer@gmail.com>
2023-03-21 18:40:05 +0200

    adding swagger

--- omniverse-server/src/app.controller.ts
+++ omniverse-server/src/app.controller.ts
@@ -1,7 +1,19 @@
-import { Body, Controller, Delete, Get, Patch, Post, Req, } from '@nestjs/common'
+import { Body, Controller, Delete, Get, Post, } from '@nestjs/common'
 import { AppService } from './app.service'
 import { Todo } from './entities/todo.entity'
-import { Request } from 'express'
+import { ApiTags, ApiBody } from '@nestjs/swagger'
+
+const todoSchema = {
+  properties: {
+    'todo': { properties: {
+        'id': { type: 'number' },
+        'title': { type: 'string' },
+        'order': { type: 'number' },
+        before: { type: 'number'},
+        after: { type: 'number'},
+      } },
+  }
+}
 
 const cloneTodo = (todo:Todo):Todo => {
   const newTodo = new Todo()
@@ -10,6 +22,7 @@ const cloneTodo = (todo:Todo):Todo => {
   })
   return newTodo
 }
+@ApiTags('api/todos')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -23,19 +36,34 @@ export class AppController {
   async getTodos(): Promise<Todo[]> {
     return await this.appService.findAll()
   }
+
   @Post('api/todos')
-  async appendTodo(@Req() req:Request, @Body('todo') todo:Todo): Promise<Todo> {
+  @ApiBody({
+    schema: todoSchema
+  })
+  async appendTodo(@Body('todo') todo:Todo): Promise<Todo> {
     const todoEntity = cloneTodo(todo)
     return await this.appService.appendTodo(todoEntity)
   }
 
   @Delete('api/todos')
+  @ApiBody({
+    schema: todoSchema
+  })
   async deleteTodo(@Body('todo') todo:Todo) {
     const todoEntity = cloneTodo(todo)
     return await this.appService.deleteTodo(todoEntity)
   }
 
   @Post('api/todos/relocate')
+  @ApiBody({
+    schema: {
+      properties: {
+        todo: todoSchema.properties.todo,
+        placeBefore: { type: 'number' }
+      }
+    }
+  })
   async relocateTodo(
       @Body('todo') todo:Todo,
       @Body('placeBefore') placeBefore:number) {
--- omniverse-server/src/main.ts
+++ omniverse-server/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+    const config = new DocumentBuilder()
+        .setTitle('Omnivers')
+        .setDescription('The todos API description')
+        .build()
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document)
+
     app.enableCors({
         origin: true,
         //preflightContinue: true,
